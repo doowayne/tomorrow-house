350a6bd840f598947405e6b1de207b324e89696e
doohwikim <doohwi0129@gmail.com>
2023-09-10 20:17:51 +0900

    🚧 Import tiny-slider.js and initialize product carousel

---
+++ js/productCarousel.js
@@ -0,0 +1,13 @@
+const productCarousel = tns({
+    container: '.product-carousel .slider-list',
+    controls: false,
+    navContainer: '.product-carousel .thumbnail-list',
+    navAsThumbnails: true,
+    arrowKeys: true,
+    autoplayHoverPause: true,
+    autoplayButtonOutput: false,
+    mouseDrag: true,
+    preventScrollOnTouch: true,
+    // slideBy: 'page',
+    // autoplay: true
+  });
